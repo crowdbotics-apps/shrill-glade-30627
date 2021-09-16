8cb9077e89f6607a19084d7276c55ea964cd5421
Crowdbotics <team@crowdbotics.com>
2021-09-16 23:43:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shrill_glade_30627Identifier",
+  url: "https://shrill_glade_30627Identifier.botics.co",
+  api: "https://shrill_glade_30627Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
